2e366dd504b83413377640555fb4e1cac2d229df
vamshithallapalli <thallapallivamshi50@gmail.com>
2025-04-02 21:48:22 +0530

    added all files

---
+++ src/components/Projects/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have had the privilege of working on a variety of projects that
+          reflect my technical expertise and creativity. Here are some
+          highlights:
+        </Desc>
+
+        <CardContainer>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
